35693eca0e47e5ee8ba039e19a5475c101578b28
Tingfang Bao <bbao@pivotal.io>
2020-03-11 09:53:34 +0800

    Set GPHOME to the dir which grrenplum_path.sh located
       * Set GPHOME to `greenplum_path.sh` script dir
       * Remove `symlink` from greenplum_path.sh
    
    Authored-by: Tingfang Bao <bbao@pivotal.io>

--- gpMgmt/bin/generate-greenplum-path.sh
+++ gpMgmt/bin/generate-greenplum-path.sh
@@ -1,26 +1,15 @@
 #!/usr/bin/env bash
 
 if [ x$1 != x ] ; then
-    GPHOME_PATH=$1
+    GPHOME_PATH="$1"
 else
-    GPHOME_PATH="\`pwd\`"
+    GPHOME_PATH="\$( cd \"\$( dirname \"\${BASH_SOURCE[0]}\" )\" >/dev/null 2>&1 && pwd )"
 fi
 
-if [ "$2" = "ISO" ] ; then
-	cat <<-EOF
-		if [ "\${BASH_SOURCE:0:1}" == "/" ]
-		then
-		    GPHOME=\`dirname "\$BASH_SOURCE"\`
-		else
-		    GPHOME=\`pwd\`/\`dirname "\$BASH_SOURCE"\`
-		fi
-	EOF
-else
-	cat <<-EOF
-		#!/usr/bin/env bash
-		GPHOME=${GPHOME_PATH}
-	EOF
-fi
+cat <<-EOF
+#!/usr/bin/env bash
+GPHOME="${GPHOME_PATH}"
+EOF
 
 
 PLAT=`uname -s`
@@ -29,56 +18,40 @@ if [ $? -ne 0 ] ; then
     exit 1
 fi
 
-cat << EOF
-
-# Replace with symlink path if it is present and correct
-if [ -h \${GPHOME}/../greenplum-db ]; then
-    GPHOME_BY_SYMLINK=\$(cd \${GPHOME}/../greenplum-db/ && pwd -P)
-    if [ x"\${GPHOME_BY_SYMLINK}" = x"\${GPHOME}" ]; then
-        GPHOME=\$(cd \${GPHOME}/../greenplum-db/ && pwd -L)/.
-    fi
-    unset GPHOME_BY_SYMLINK
-fi
-EOF
-
 cat <<EOF
 
 PYTHONHOME="\${GPHOME}/ext/python"
-export PYTHONHOME
-PYTHONPATH=\${GPHOME}/lib/python
-
-PATH=\${GPHOME}/bin:\${PYTHONHOME}/bin:\${PATH}
-
-LD_LIBRARY_PATH=\${GPHOME}/lib:\${PYTHONHOME}/lib:\${LD_LIBRARY_PATH-}
-export LD_LIBRARY_PATH
+PYTHONPATH="\${GPHOME}/lib/python"
+PATH="\${GPHOME}/bin:\${PYTHONHOME}/bin:\${PATH}"
+LD_LIBRARY_PATH="\${GPHOME}/lib:\${PYTHONHOME}/lib\${LD_LIBRARY_PATH:+:\$LD_LIBRARY_PATH}"
 EOF
 
 # AIX uses yet another library path variable
 # Also, Python on AIX requires special copies of some libraries.  Hence, lib/pware.
 if [ "${PLAT}" = "AIX" ]; then
 cat <<EOF
-PYTHONPATH=\${GPHOME}/ext/python/lib/python2.7:\${PYTHONPATH}
-LIBPATH=\${GPHOME}/lib/pware:\${GPHOME}/lib:\${GPHOME}/ext/python/lib:/usr/lib/threads:\${LIBPATH}
+PYTHONPATH="\${GPHOME}/ext/python/lib/python2.7:\${PYTHONPATH}"
+LIBPATH="\${GPHOME}/lib/pware:\${GPHOME}/lib:\${GPHOME}/ext/python/lib:/usr/lib/threads:\${LIBPATH}"
 export LIBPATH
-GP_LIBPATH_FOR_PYTHON=\${GPHOME}/lib/pware
+GP_LIBPATH_FOR_PYTHON="\${GPHOME}/lib/pware"
 export GP_LIBPATH_FOR_PYTHON
 EOF
 fi
 
 # openssl configuration file path
 cat <<EOF
+
 if [ -e "\$GPHOME/etc/openssl.cnf" ]; then
-OPENSSL_CONF=\$GPHOME/etc/openssl.cnf
-export OPENSSL_CONF
+   OPENSSL_CONF="\$GPHOME/etc/openssl.cnf"
 fi
 EOF
 
 cat <<EOF
+
 export GPHOME
 export PATH
-EOF
-
-cat <<EOF
+export PYTHONHOME
 export PYTHONPATH
+export LD_LIBRARY_PATH
+export OPENSSL_CONF
 EOF
-
